210331a2e4e4e7c8028a05ca29fd99faca581182
Siarhei Pratasavitski <>
2024-10-03 13:04:51 +0200

    First commit

---
+++ Tabbedapp/Tabbedapp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Tabbedapp.Models;
+using Tabbedapp.ViewModels;
+
+namespace Tabbedapp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
